f9dccb2f7e9cbf357decca5d939c3418c8c4e218
ahmedbenarab <ahmedbenarab@hotmail.fr>
2021-12-18 20:44:42 +0100

    we brought more stability and we did some bug fix

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ it's simple , we take the weight and the height then we applie the function bell
 
  ## the aim of this app
 
- - [X] show the result of a bmi in clear font 
+ - [X] ~~show the result of a bmi in clear font~~
  - [ ] add graph for normal and abnormal values
  - [ ] add clear input boxes
  - [ ] focusing on aesthetics 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function clcBmi() {
+    let name = document.getElementById('name').value ;
+    let surname = document.getElementById('surname').value ;
     let h_t = document.getElementById("height").value ;
     let w_t = document.getElementById("weight").value ;
     let htsqr = h_t**2 ;
     let bmiValue = (w_t/htsqr); 
+    let result = `hey ${name} ${surname} your bmi is ${bmiValue} , have a nice day bruh`
     
-    alert(bmiValue) ;
+    //alert(result) ;
+    document.getElementById('result').textContent = result
 }
