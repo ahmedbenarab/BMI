6767e13b5d6baca82cd6ac5b2d22a3a3b9fa26df
ahmedbenarab <ahmedbenarab@hotmail.fr>
2021-12-19 01:04:13 +0100

    Aesthetique

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 function clcBmi() {
-    let name = document.getElementById('name').value ;
-    let surname = document.getElementById('surname').value ;
-    let h_t = document.getElementById("height").value ;
-    let w_t = document.getElementById("weight").value ;
-    let htsqr = h_t**2 ;
-    let bmiValue = (w_t/htsqr); 
-    let result = `hey ${name} ${surname} your bmi is ${bmiValue} , have a nice day bruh`
-    
+    let name = document.getElementById('name').value;
+    let surname = document.getElementById('surname').value;
+    let h_t = document.getElementById("height").value;
+    let w_t = document.getElementById("weight").value;
+    let htsqr = h_t ** 2;
+    let bmiValue = (w_t / htsqr);
+    let result = `hey ${name} ${surname} your bmi is ${bmiValue} , have a nice day bruh 🙂`
+
     //alert(result) ;
-    document.getElementById('result').textContent = result
+    document.getElementById('content').textContent = result
+        //document.getElementsByClassName('content').textContent = result
 }
